6a5ee250620257efda12636307b3eb7047afc721
miandry <miandrilala9@yahoo.fr>
2025-09-06 09:57:50 +0300

    save

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://miandrilala.online',
+  baseURL: window.SERVER_URL,
   headers: { 'Content-Type': 'application/json' }
 })
 
